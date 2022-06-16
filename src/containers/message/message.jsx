705cfafd6ca7406ea94573f86b5e32facd6e7b3d
linweifan <1763977795@qq.com>
2022-06-16 11:14:01 +0800

    CICD1

--- src/containers/message/message.jsx
+++ src/containers/message/message.jsx
@@ -56,7 +56,7 @@ class Message extends Component {
                 {lastMsgs.map(msg => {
                     const targetUserId = msg.to === user._id ? msg.from : msg.to
                     const targetUser = msg.to === user._id ? users[msg.from] : users[msg.to]
-                    console.log("testtttaAA")
+                    console.log("testtttaAAb")
                     console.log(users)
                     console.log(targetUser)
 
